7c433891ac45d2272109963319f06fb144d14526
mohmmdgaber <mohmmd.gaber@gmail.com>
2022-04-10 20:12:31 +0300

    css fix

--- School/settings.py
+++ School/settings.py
@@ -116,7 +116,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_PORT = 587
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
