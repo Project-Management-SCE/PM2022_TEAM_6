28fdf63ed7cba50e72b551953a4ba3b9b27d6855
hamadabosbet <Hamad1997m@gmail.com>
2022-04-12 14:01:18 -0700

    BSPM2022T6-130 BSPM2022T6-128 BSPM2022T6-129 BSPM2022T6-127

--- manger/models.py
+++ manger/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class messegerequest(models.Model):
+    text=models.CharField(max_length=400)
+    header=models.CharField(max_length=200)
+    urg=models.BooleanField()
+    volid=models.IntegerField(default=-1)
+    def __str__(self):
+        return self.text
+    def __str__(self):
+        return self.urg
