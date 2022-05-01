5ce35d464919fc5026e2c5ae1d95db1189d55cd3
mohmmdgaber <mohmmd.gaber@gmail.com>
2022-05-02 00:02:09 +0300

    BSPM2022T6-86

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("contactus/", views.contact_us, name="contactus"),
+    path("aboutus/", views.about_us, name="aboutus"),
 
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -20,3 +20,8 @@ def contact_us(response):
         message='Your message was sent to the Admin'
 
     return render(response, "mainpage/contactus.html", {'message':message})
+
+def about_us(response):
+    mainbody='Oh feel if up to till like. He an thing rapid these after going drawn or. Timed she his law the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant formerly so ye blessing.'
+    quote='He as spoke avoid given downs money on we. Of properly carriage shutters ye as wandered up repeated moreover.'
+    return render(response, "mainpage/About_us.html", {'mainbody':mainbody,'quote':quote})
