6dcfc1cc9bd366d9b6f15bcd6c922939c85b8afa
hamadabosbet <hamad1997m@gmail.com>
2022-05-15 15:55:31 -0700

    upload

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ from django.core.management.commands.runserver import Command as runserver
 port = int(os.environ.get("PORT", 8080))
 runserver.default_port = port
 runserver.default_addr='0.0.0.0'
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'School.settings')
     try:
